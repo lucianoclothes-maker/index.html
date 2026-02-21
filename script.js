ab5522aeead766af0e122753803d86a084d091a5
global-ops <lucianoclothes@gmail.com>
2026-02-21 15:48:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -109,7 +109,7 @@ fetch('https://raw.githubusercontent.com/datasets/geo-boundaries-world-110m/mast
         { name: "Incirlik Air Base (NATO)", type: "us-air", lat: 37.00, lon: 35.42 },
         { name: "Aviano Air Base (Italy)", type: "us-air", lat: 46.03, lon: 12.59 },
         { name: "Diego Garcia Base", type: "us-naval", lat: -7.31, lon: 72.41 },
-        { name: "Kaliningrad HQ", type: "ru-hq", lat: 54.71, lon: 20.45 }
+        { name: "Kaliningrad HQ", type: "ru-hq", lat: 54.71, lon: 20.45 },
         { name: "Muwaffaq Salti Air Base (Jordan)", type: "us-air", lat: 31.83, lon: 36.78 },
     ];
 
